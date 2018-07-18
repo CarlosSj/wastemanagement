fb8a150733f12d68db58d30fd00fa9e57b08554e
ashabooga <bcuellowolffe@gmail.com>
2018-07-18 11:48:37 -0400

    fixed bugs & started on javascript code

---
+++ script.js
@@ -0,0 +1,10 @@
+/*window.onscroll = function() {myFunction()};
+var titles = document.getElementById("homeTreeImage");
+var sticky = titles.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    titles.classList.add("sticky")
+  } else {
+    titles.classList.remove("sticky");
+  }
+}
