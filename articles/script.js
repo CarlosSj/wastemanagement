c596ad98bfc959b0fdeb869078fcaefe7eabb090
ashabooga <bcuellowolffe@gmail.com>
2018-07-18 15:00:59 -0400

    added showcase to articles page

---
+++ articles/script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+    var nav = document.getElementById('titles');
+    if ( window.pageYOffset > 100 ) {
+        nav.classList.add("titles1");
+    } else {
+        nav.classList.remove("titles1");
+    }
+}
+
